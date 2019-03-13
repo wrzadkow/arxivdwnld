5bc53b15cfcfad4232b9262a09e2790b72c897a8
Wojciech Rzadkowski <wojciech.rzadkowski@gmail.com>
2019-03-13 17:40:16 +0100

    Added support for old identifiers (field/number)

--- arxivdwnld.py
+++ arxivdwnld.py
@@ -3,17 +3,22 @@ import glob,os,re,subprocess, sys
 #takes an Arxiv ID as argument, downloads and unpacks the corresponding paper
 def DownloadAndUnpack(id):
 	print('opening directory for paper '+str(id))
-	os.system('mkdir '+str(id))
-	os.system('wget arxiv.org/e-print/'+str(id)+
+	idreplaced=str(id).replace('/', '') #for better handling of old arxiv IDs
+	os.system('mkdir '+idreplaced)
+	os.system('wget arxiv.org/e-print/'+str(id)+' --output-document='+idreplaced+'downloaded'+
 				' --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" -P '
 				+str(id))
-	filetype = subprocess.check_output("file "+str(id)+'/'+str(id), shell=True)
-	filetype=filetype.decode("utf-8") #convert bytes object to string
+	filetype = subprocess.check_output("file "+idreplaced+'downloaded', shell=True)
+	filetype = filetype.decode("utf-8") #convert bytes object to string
+	print('filetype retrieved: \n',filetype)	
 	#to parse the 'file' command output, we need position of filetype in the returned string
-	pos=2*len(str(id))+3
+	pos=len(idreplaced+'downloaded')+2
+	print('filetype in position', filetype[pos:pos+4])	
 	if (filetype[pos:pos+4]=='gzip'):
 		print('uncompressing') 
-		os.system('tar -xvzf '+str(id)+'/'+str(id)+' -C '+str(id))
+		command='tar -xvzf '+idreplaced+'downloaded -C '+idreplaced
+		print('command',command)
+		os.system(command)
 	elif(filetype[pos:pos+3]=='PDF'):
 		print('the paper was in PDF format, no uncompressing needed')
 	else:
